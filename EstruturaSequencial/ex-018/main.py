01460d0cac87dfef538039a924d7fac3bd82b662
humbertoluis22 <humbertoluis2013@gmail.com>
2024-07-19 01:46:52 -0300

    incluindo exercicios

--- EstruturaSequencial/ex-018/main.py
+++ EstruturaSequencial/ex-018/main.py
@@ -1,6 +1,10 @@
-tamanho_arquivo = int(input('informe o tamanho do arquivo para dowload em MB: '))
-velocidade_internet = int(input('informe a velocidade de um link de internet em Mbps: '))
+tamanho_arquivo = int(input("informe o tamanho do arquivo para dowload em MB: "))
+velocidade_internet = int(
+    input("informe a velocidade de um link de internet em Mbps: ")
+)
 
-tempo_segundo  = round(tamanho_arquivo * 8 ) / (velocidade_internet)   
+tempo_segundo = round(tamanho_arquivo * 8) / (velocidade_internet)
 
-print(f'Para baixar um arquivo de {tamanho_arquivo} MB ira levar {tempo_segundo:.2f} segundos')
+print(
+    f"Para baixar um arquivo de {tamanho_arquivo} MB ira levar {tempo_segundo:.2f} segundos"
+)
